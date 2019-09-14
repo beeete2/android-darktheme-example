e022026b8766748baea35dba8b5e26fccf4d3cf7
beeete2 <beeete2@gmail.com>
2019-09-14 14:09:00 +0900

    add Bottom Navigation

---
+++ app/src/main/java/com/beeete2/android/examples/darktheme/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.beeete2.android.examples.darktheme.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/beeete2/android/examples/darktheme/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.beeete2.android.examples.darktheme.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
